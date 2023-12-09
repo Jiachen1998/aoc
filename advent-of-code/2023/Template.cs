8820efea8477e2f577d265e494407fd1c2104b46
Jiachen Lu <jiachen.lu@hotmail.com>
2023-12-09 20:58:48 +1100

    Day 9 workinng

--- advent-of-code/2023/Template.cs
+++ advent-of-code/2023/Template.cs
@@ -6,12 +6,12 @@
         private static readonly string Input = "";
         private static List<string> _lines = Utilities.StringToLines(Input);
 
-        private static int Part1()
+        internal static int Part1()
         {
             return 0;
         }
 
-        private static int Part2()
+        internal static int Part2()
         {
             return 0;
         }
--- advent-of-code/2023/Utilities.cs
+++ advent-of-code/2023/Utilities.cs
@@ -6,5 +6,48 @@
         {
             return str.Split("\r\n").ToList();
         }
+
+        /// <summary>
+        /// Iteration to calculate LCM of multiple numbers
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        internal static long CalculateLcmMultiple(long[] nums)
+        {
+            long lcm = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                lcm = CalculateLcm(lcm, nums[i]);
+            }
+            return lcm;
+        }
+
+        /// <summary>
+        /// Formula to calculate LCM of two numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        internal static long CalculateLcm(long a, long b)
+        {
+            return a / CalculateGcd(a, b) * b;
+        }
+
+        /// <summary>
+        /// Formula to calculate GCD
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        internal static long CalculateGcd(long a, long b)
+        {
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
     }
 }
